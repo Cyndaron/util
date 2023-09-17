e9e159908acd01d6b20455a748b27a13595ea88f
Michael Steenbeek <m.o.steenbeek@gmail.com>
2023-09-17 18:29:50 +0200

    Sync with main project

---
+++ src/DependencyInjectionContainer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © 2009-2020 Michael Steenbeek
+ *
+ * Cyndaron is licensed under the MIT License. See the LICENSE file for more details.
+ */
+declare(strict_types=1);
+
+namespace Cyndaron\Util;
+
+use function get_class;
+
+final class DependencyInjectionContainer
+{
+    private array $objects = [];
+
+    public function add(object $object): void
+    {
+        $className = get_class($object);
+        $this->objects[$className] = $object;
+    }
+
+    public function get(string $className): ?object
+    {
+        return $this->objects[$className] ?? null;
+    }
+}
